66471f496bec0deb89285d9bc8a34df29dbe19e1
weicl <chenglong.wei@mail.hypers.com>
2022-03-18 18:15:44 +0800

    api 中添加 exceptionHandler 对象

--- src/main/scala/com/github/yjgbg/vertx/boot/api.scala
+++ src/main/scala/com/github/yjgbg/vertx/boot/api.scala
@@ -9,6 +9,7 @@ trait api extends valid.CoreSyntax:
   type HttpServerConfig = http.server.HttpServerConfig
   type Controller[A, B] = http.server.Controller[A, B]
   type Middleware = http.server.Middleware
+  type ExceptionHandler[A <: Throwable] = http.server.ExceptionHandler[A]
   type HttpRequest[A] = http.server.HttpRequest[A]
   type HttpResponse[A] = http.server.HttpResponse[A]
   type Method = http.server.Method
@@ -23,6 +24,7 @@ trait api extends valid.CoreSyntax:
   val VerticleBean: core.VerticleBean.type = core.VerticleBean
   val Controller: http.server.Controller.type = http.server.Controller
   val Middleware: http.server.Middleware.type = http.server.Middleware
+  val ExceptionHandler:http.server.ExceptionHandler.type =http.server.ExceptionHandler
   val HttpRequest: http.server.HttpRequest.type = http.server.HttpRequest
   val HttpResponse: http.server.HttpResponse.type = http.server.HttpResponse
   val VertxCpsMonad: support.VertxCpsMonad.type = support.VertxCpsMonad
