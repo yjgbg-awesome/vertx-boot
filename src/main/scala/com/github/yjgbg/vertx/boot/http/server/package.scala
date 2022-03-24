30ca119feebac86eaf53c2e487a308b0436e1a0e
weicl <chenglong.wei@mail.hypers.com>
2022-03-25 03:57:33 +0800

    处理了重复向response中写入数据的问题：因为在写入数据之后又发生了异常，触发了异常处理逻辑，所以再次尝试写入数据了

--- src/main/scala/com/github/yjgbg/vertx/boot/http/server/package.scala
+++ src/main/scala/com/github/yjgbg/vertx/boot/http/server/package.scala
@@ -5,7 +5,8 @@ import io.vertx.core.Future
 import io.vertx.ext.web.RoutingContext
 
 import scala.reflect.ClassTag
-
+import cps.{async}
+import vertxCps.CpsSyntax.given
 package object server:
   type Method = "GET" | "POST" | "PUT" | "DELETE" | "OPTION" | "PATCH"
   type RequestLine = (Method, String)
@@ -16,22 +17,24 @@ package object server:
   type ResponseHeaders = Map[String, String]
   type HttpResponse[A] = (HttpStatus, ResponseHeaders, Option[A])|Option[A]|A
   extension [A] (httpResponse:HttpResponse[A])
-    def use(ctx: RoutingContext)(using encoder: io.circe.Encoder[A]):Future[Unit] = httpResponse match 
-      case (status: HttpStatus,headers,Some(body)) =>
-        ctx.response().setStatusCode(status)
-        headers.asInstanceOf[ResponseHeaders].foreach(ctx.response().putHeader(_,_))
-        ctx.response().end(encoder(body.asInstanceOf[A]).noSpaces).asInstanceOf
-      case (status: HttpStatus,headers,None) =>
-        ctx.response().setStatusCode(status)
-        headers.asInstanceOf[ResponseHeaders].foreach(ctx.response().putHeader(_,_))
-        ctx.response().end().asInstanceOf
-      case Some(body) => ctx.response().setStatusCode(200)
-        .putHeader("ContentType","application/json")
-        .end(encoder(body.asInstanceOf[A]).noSpaces).asInstanceOf
-      case None => ctx.response().setStatusCode(200).end().asInstanceOf
-      case body => ctx.response().setStatusCode(200)
-        .putHeader("ContentType","application/json")
-        .end(encoder(body.asInstanceOf[A]).noSpaces).asInstanceOf
+    def use(ctx: RoutingContext)(using encoder: io.circe.Encoder[A]):Future[Unit] = async {
+      httpResponse match
+        case (status: HttpStatus,headers,Some(body)) =>
+          ctx.response().setStatusCode(status)
+          headers.asInstanceOf[ResponseHeaders].foreach(ctx.response().putHeader(_,_))
+          ctx.response().end(encoder(body.asInstanceOf[A]).noSpaces)
+        case (status: HttpStatus,headers,None) =>
+          ctx.response().setStatusCode(status)
+          headers.asInstanceOf[ResponseHeaders].foreach(ctx.response().putHeader(_,_))
+          ctx.response().end()
+        case Some(body) => ctx.response().setStatusCode(200)
+          .putHeader("ContentType","application/json")
+          .end(encoder(body.asInstanceOf[A]).noSpaces)
+        case None => ctx.response().setStatusCode(200).end()
+        case body => ctx.response().setStatusCode(200)
+          .putHeader("ContentType","application/json")
+          .end(encoder(body.asInstanceOf[A]).noSpaces)
+    }
   object HttpResponse:
     def apply[A](status: HttpStatus = 200,
                  header: ResponseHeaders = Map(),
