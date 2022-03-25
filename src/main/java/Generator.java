6ff351510379bcacfe42241d7c0d606fb3be602b
weicl <chenglong.wei@mail.hypers.com>
2022-03-25 19:55:56 +0800

    yaml文件提示

--- src/main/java/Generator.java
+++ src/main/java/Generator.java
@@ -3,7 +3,10 @@
 //import io.vertx.core.http.HttpServerOptions;
 //import io.vertx.ext.web.client.WebClientOptions;
 //import io.vertx.micrometer.MicrometerMetricsOptions;
+//import io.vertx.mysqlclient.MySQLConnectOptions;
 //import io.vertx.redis.client.RedisOptions;
+//import io.vertx.sqlclient.PoolOptions;
+//import org.springframework.aop.target.PoolingConfig;
 //import org.springframework.boot.context.properties.ConfigurationProperties;
 //import org.springframework.context.annotation.Bean;
 //
@@ -36,5 +39,15 @@
 //  @ConfigurationProperties("vertx-boot.redis.options")
 //  @Bean public RedisOptions redisOptions() {
 //    return new RedisOptions();
+//  }
+//
+//  @ConfigurationProperties("vertx-boot.mysql.connection-options")
+//  @Bean public MySQLConnectOptions mySQLConnectOptions() {
+//    return new MySQLConnectOptions();
+//  }
+//
+//  @ConfigurationProperties("vertx-boot.mysql.pool-options")
+//  @Bean public PoolOptions poolOptions() {
+//    return new PoolOptions();
 //  }
 //}
