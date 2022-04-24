6ea7de1176a73ec618b0acac0ebfe8838e5f6488
weicl <chenglong.wei@mail.hypers.com>
2022-04-24 14:58:38 +0800

    版本更新到1.3 更容易地接入监控 by micrometer

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 group = "com.github.yjgbg"
 java.sourceCompatibility = JavaVersion.VERSION_11
-version = "1.2"
+version = "1.3"
 
 repositories {
     mavenLocal()
--- src/main/scala/com/github/yjgbg/vertx/boot/core/CoreConfig.scala
+++ src/main/scala/com/github/yjgbg/vertx/boot/core/CoreConfig.scala
@@ -3,14 +3,17 @@ package core
 
 import com.typesafe.scalalogging.Logger
 import io.micrometer.core.instrument.MeterRegistry
+import io.micrometer.core.instrument.binder.MeterBinder
 import io.micrometer.core.instrument.composite.CompositeMeterRegistry
 import io.micrometer.core.instrument.simple.SimpleMeterRegistry
 import io.vertx.core.{Vertx, VertxOptions}
 import io.vertx.micrometer.MicrometerMetricsOptions
+import io.vertx.core.metrics.MetricsOptions
+import io.vertx.micrometer.backends.BackendRegistries
 import org.springframework.beans.factory.InitializingBean
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.boot.autoconfigure.condition.{ConditionalOnBean, ConditionalOnProperty}
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.context.annotation.{Bean, Primary}
+import org.springframework.context.annotation.{Bean, DependsOn, Primary}
 
 import java.util.Optional
 
@@ -19,16 +22,15 @@ lazy val log: Logger = Logger[CoreConfig]
 trait CoreConfig:
   @ConfigurationProperties("vertx-boot.core.vertx-options.metrics-options")
   @ConditionalOnProperty(name = Array("vertx-boot.core.vertx-options.metrics-options.enabled"), havingValue = "true")
-  @Bean def metricsOptions(meterRegistryList: java.util.List[MeterRegistry]): MicrometerMetricsOptions = {
-    val compositeMeterRegistry = CompositeMeterRegistry()
-    meterRegistryList.forEach(compositeMeterRegistry.add(_))
-    new MicrometerMetricsOptions().setMicrometerRegistry(compositeMeterRegistry)
-  }
-  @ConditionalOnProperty(name = Array("vertx-boot.core.vertx-options.metrics-options.enabled"), havingValue = "true")
-  @Bean @Primary def simpleMeterRegistry:MeterRegistry = new SimpleMeterRegistry()
+  @Bean def metricsOptions: MetricsOptions = MicrometerMetricsOptions()
+
+  @DependsOn(Array("vertx"))
+  @ConditionalOnBean(Array(classOf[MetricsOptions]))
+  @Bean def processingAllMeterBinder(meterBinder: java.util.List[MeterBinder]):InitializingBean = () =>
+    meterBinder.forEach(_.bindTo(BackendRegistries.getDefaultNow))
 
   @ConfigurationProperties("vertx-boot.core.vertx-options")
-  @Bean def vertxOptions(metricsOptions: Optional[MicrometerMetricsOptions]): VertxOptions =
+  @Bean def vertxOptions(metricsOptions: Optional[MetricsOptions]): VertxOptions =
     if metricsOptions.isEmpty then VertxOptions() else VertxOptions().setMetricsOptions(metricsOptions.get())
 
   @Bean def vertx(vertxOptions: VertxOptions): Vertx = {
